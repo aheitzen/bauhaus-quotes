7b0c46536d46ce7add2ccaad3b5742ce3dd230b0
Alexa Heitzenrader <aheitzen1@gmail.com>
2018-05-29 17:16:31 -0700

    day 6 start

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	$('#multiscroll').multiscroll({
 	navigation: true,
-	anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage', 'lastPage'],
+	anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage', 'sixthPage', 'lastPage'],
 	menu: '#myMenu',
 	verticalCentered : false
 
