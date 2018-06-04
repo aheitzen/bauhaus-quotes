2e8b1906c3753da85e7bc6fd58770881cced4a73
Alexa Heitzenrader <aheitzen1@gmail.com>
2018-06-03 17:56:11 -0700

    day eight text

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	$('#multiscroll').multiscroll({
 	navigation: true,
-	anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage', 'sixthPage', 'seventhPage', 'lastPage'],
+	anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage', 'sixthPage', 'seventhPage', 'eigthPage', 'lastPage'],
 	menu: '#myMenu',
 	verticalCentered : false
 
