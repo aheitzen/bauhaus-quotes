3b89b42a570c9123c2c99083982eb782a8a08db6
Alexa Heitzenrader <aheitzen1@gmail.com>
2018-06-05 17:44:53 -0700

    day 9 and 10 structure

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	$('#multiscroll').multiscroll({
 	navigation: true,
-	anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage', 'sixthPage', 'seventhPage', 'eigthPage', 'lastPage'],
+	anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage', 'sixthPage', 'seventhPage', 'eigthPage', 'ninthPage', 'tenthPage', 'lastPage'],
 	menu: '#myMenu',
 	verticalCentered : false
 
