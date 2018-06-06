0085b632068e60bb00204a805bfba7776166eead
Alexa Heitzenrader <aheitzen1@gmail.com>
2018-06-05 23:31:26 -0700

    final commit

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
 	$('#multiscroll').multiscroll({
-	navigation: true,
-	anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage', 'sixthPage', 'seventhPage', 'eigthPage', 'ninthPage', 'tenthPage', 'lastPage'],
-	menu: '#myMenu',
-	verticalCentered : false
-
+		navigation: true,
+		anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage', 'sixthPage', 'seventhPage', 'eigthPage', 'ninthPage', 'tenthPage', 'lastPage'],
+		menu: '#myMenu',
+		verticalCentered : false
 	});
 });
